c88b8fa8045892e6992495e132687455e6b99fef
Cristopher Escorcia <cristopher.escorcia@condorlabs.io>
2022-10-28 18:34:33 -0500

    Initial Commit

---
+++ Lulo-technical-test/Lulo_technical_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lulo_technical_testApp.swift
+//  Lulo-technical-test
+//
+//  Created by Cristopher Escorcia on 28/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Lulo_technical_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
